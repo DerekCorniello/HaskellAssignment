a788329d40e0138d9ae82352072081dac7a6edde
XDCoolMan <dcorniello.business@gmail.com>
2023-11-27 20:43:55 -0500

    Assignment finished

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -8,8 +8,20 @@ instance Show Found where
   show (Match index) = "Found match at " ++ show index
   show NoMatch = "No match found!"
 findFirst :: Eq a => (a -> Bool) -> [a] -> Found
-
+findFirst needle haystack = findHelper needle haystack 0
+  where
+    findHelper _ [] _ = NoMatch
+    findHelper needle (hay:stack) index =
+      if needle hay
+        then Match index
+        else findHelper needle stack (index + 1)
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
 palindrome :: [Char] -> Bool
+palindrome [] = True
+palindrome [x] = True  -- Single character is a palindrome
+palindrome candidate =
+  if head candidate == last candidate
+    then palindrome (init (tail candidate))
+    else False
